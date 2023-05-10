fc4e77a449af273bdaeaf357cc5e4868a4ef1e08
Jéssica <pedroso.jeh12@gmail.com>
2023-05-10 18:37:58 -0300

    modificado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
-import ListaSuspensa from './componentes/ListaSuspensa';
+
 
 
 const App = () => {
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -10,6 +10,12 @@ const Formulario = () => {
         'Pet'
         ]
 
+        const [nome, setNome] = useState = ('')
+        const [profissao, setProfissao] = useState = ('')
+        const [imagem, setImagem] = useState = ('')
+
+        
+
 const aoSalvar = (evento) => {
     evento.preventDefault()
 }
@@ -18,9 +24,26 @@ const aoSalvar = (evento) => {
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha o campos para cadastrar um membro de sua família.</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome.."/>
-                <CampoTexto obrigatorio={true} label="Profissão" placeholder="Digite seu cargo.."/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem.."/>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome.."
+                    valor={nome}
+                    aoAlterado={valor => setNome (valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Profissão" 
+                    placeholder="Digite seu cargo.."
+                    valor={profissao}
+                    aoAlterado={valor => setProfissao(valor)}
+                />
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem.."
+                    valor={imagem}
+                    aoAlterado={valor => setImagem (valor)}
+                />
                 <ListaSuspensa obrigatorio={true} label="Quem é você?" itens={times}/>
                 <Botao>
                     Criar cards
