cdb1e0327d0c0bd61d7723cd408fc85889842886
Jéssica <pedroso.jeh12@gmail.com>
2023-05-20 21:57:10 -0300

    projeto final

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,56 +1,67 @@
-import Botao from "../Botao";
-import CampoTexto from "../CampoTexto";
-import ListaSuspensa from "../ListaSuspensa";
-import "./Formulario.css";
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
 
-const Formulario = () => {
-    
-    const times = [
-        'Tutora',
-        'Pet'
-        ]
+const Formulario = (props) => {
 
-        const [nome, setNome] = useState = ('')
-        const [profissao, setProfissao] = useState = ('')
-        const [imagem, setImagem] = useState = ('')
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
 
-        
-
-const aoSalvar = (evento) => {
-    evento.preventDefault()
-}
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
+    }
 
     return (
         <section className="formulario">
             <form onSubmit={aoSalvar}>
-                <h2>Preencha o campos para cadastrar um membro de sua família.</h2>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto 
-                    obrigatorio={true} 
-                    label="Nome" 
-                    placeholder="Digite seu nome.."
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome" 
                     valor={nome}
-                    aoAlterado={valor => setNome (valor)}
+                    aoAlterado={valor => setNome(valor)}
                 />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Profissão" 
-                    placeholder="Digite seu cargo.."
-                    valor={profissao}
-                    aoAlterado={valor => setProfissao(valor)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo" 
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
                 />
-                <CampoTexto 
-                    label="Imagem" 
-                    placeholder="Digite o endereço da imagem.."
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem" 
                     valor={imagem}
-                    aoAlterado={valor => setImagem (valor)}
+                    aoAlterado={valor => setImagem(valor)}
                 />
-                <ListaSuspensa obrigatorio={true} label="Quem é você?" itens={times}/>
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time" 
+                    itens={props.times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                />
                 <Botao>
-                    Criar cards
+                    Criar Card
                 </Botao>
             </form>
         </section>
     )
 }
 
-export default Formulario;
+export default Formulario
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,14 +1,16 @@
-import "./ListaSuspensa.css";
+import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
     return (
-        <div className="lista-suspensa" > 
+        <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio}>
-                {props.itens.map(item => 
-                    {return <option key={item}> {item} </option> })}
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+                <option value=""></option>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
             </select>
-        </div> 
+        </div>
     )
 }
 
